1ab5389420a0a42d25bedeff3ca4f28bd0cda61c
Cassio Trindade <cassiowt@gmail.com>
2016-08-30 09:27:32 -0300

    Criado a tela de menu e a tela inicial de cadastro de dono.

--- src/managedbean/DonoMB.java
+++ src/managedbean/DonoMB.java
@@ -1,5 +1,30 @@
 package managedbean;
 
+import java.util.ArrayList;
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import model.Dono;
+
+@ManagedBean
+@SessionScoped
 public class DonoMB {
+	
+	private Dono dono;
+	private ArrayList<Dono> donos;
+	
+	public DonoMB() {
+		
+	}
+	
+	public Dono getDono() {
+		return dono;
+	}
+	public void setDono(Dono dono) {
+		this.dono = dono;
+	}
+	
+	
 
 }
